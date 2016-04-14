d2fbb96332add042af9392f9958205c5b3b737b6
simaxuaner <yunjia_feng2016@163.com>
2016-04-14 12:31:23 +0800

    4月14日文件夹重新整理跟新

--- sites/all/modules/friend_recommendation/friend-block.tpl.php
+++
@@ -1,3 +0,0 @@
-<ul>
-  <li><a href="node/1"><?php if ($username): print $username; endif; ?></a></li>
-</ul>
--- sites/all/modules/friend_recommendation/product-page.tpl.php
+++
@@ -1,3 +0,0 @@
-<ul>
-  <li><a href="<?php if ($productlink): print $productlink; endif; ?>"><?php if ($productname): print $productname; endif; ?></a></li>
-</ul>
