7eae1230e9983b329c95c9f56aa573d9a27f4aab
Dany <ifeifei@stu.xmu.edu.cn>
2016-04-16 12:02:41 +0800

    创建菜单使用的占位链接，内容等待补充 占位连接为 /training /training/courses /training/teachers /news /news以下等待扩充

---
+++ sites/all/modules/custom/dany/training/courses.tpl.php
@@ -0,0 +1,84 @@
+    <!--
+        使用的变量
+        imageLinkPathHead   :图像文件的头位置
+    -->
+   <a></a
+   <main id="mainSection">
+		<nav class="sideNav" id="sideNav">
+			<ul>
+				<li id="sideNavItem0" onClick="loadSubPage(0)">
+					农业课程
+				</li>
+				<li id="sideNavItem1" onClick="loadSubPage(1)">
+					农业名师
+				</li>
+			</ul>
+		</nav>
+		<content id="subPage0">
+		<h1>Featured</h1>
+		<div id="slider">
+			<img id="sliderImg" src="images/slider/2.jpg" alt="slider"/>
+		</div>		
+
+		<div class="filterBar"><h1>Classes</h1>
+			<select>
+			  <option value="volvo">Growing</option>
+			  <option value="saab">Marketing</option>
+			  <option value="mercedes">Management</option>
+			  <option value="audi">Financial</option>
+			</select>
+			<input type="text" name="firstname">
+		</div>
+		<div class="block">
+		<span class="fav">favorite</span>
+		<img src="images/farms/product-list.jpg"/>
+		<p class="productName">Fertilizers</p>
+		<p><span class="farmName">asdf</span><span class="customers">01</span><span class="comments">02</span></p>
+		</div>		
+		<div class="block">
+		<span class="fav">favorite</span>
+		<img src="images/farms/product-list.jpg"/>
+		<p class="productName">Crop-Dusting</p>
+		<p><span class="farmName">asdf</span><span class="customers">01</span><span class="comments">02</span></p>
+		</div>		
+		<div class="block">
+		<span class="fav">favorite</span>
+		<img src="images/farms/product-list.jpg"/>
+		<p class="productName">Pesticides</p>
+		<p><span class="farmName">asdf</span><span class="customers">01</span><span class="comments">02</span></p>
+		</div>		
+		<div class="block noMarginRight">
+		<span class="fav">favorite</span>
+		<img src="images/farms/product-list.jpg"/>
+		<p class="productName">Soil</p>
+		<p><span class="farmName">asdf</span><span class="customers">01</span><span class="comments">02</span></p>
+		</div>		
+		<div class="block">
+		<span class="fav">favorite</span>
+		<img src="images/farms/product-list.jpg"/>
+		<p class="productName">Time to Harvest</p>
+		<p><span class="farmName">asdf</span><span class="customers">01</span><span class="comments">02</span></p>
+		</div>		
+		<div class="block">
+		<span class="fav">favorite</span>
+		<img src="images/farms/product-list.jpg"/>
+		<p class="productName">Fertilizers</p>
+		<p><span class="farmName">asdf</span><span class="customers">01</span><span class="comments">02</span></p>
+		</div>		
+		<div class="block">
+		<span class="fav">favorite</span>
+		<img src="images/farms/product-list.jpg"/>
+		<p class="productName">Fertilizers</p>
+		<p><span class="farmName">asdf</span><span class="customers">01</span><span class="comments">02</span></p>
+		</div>		
+		<div class="block noMarginRight">
+		<span class="fav">favorite</span>
+		<img src="images/farms/product-list.jpg"/>
+		<p class="productName">Fertilizers</p>
+		<p><span class="farmName">asdf</span><span class="customers">01</span><span class="comments">02</span></p>
+		</div>		
+		</content>		
+		<content id="subPage1" class="hidden">
+		<h1>Teachers</h1>
+		</content>
+	</main>
