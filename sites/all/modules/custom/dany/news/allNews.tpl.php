df13c8d8dd2052000c53e84149d7699d42b93cf7
Dany <ifeifei@stu.xmu.edu.cn>
2016-04-17 21:12:51 +0800

    為了獲得新代嗎，提交

---
+++ sites/all/modules/custom/dany/news/allNews.tpl.php
@@ -0,0 +1,54 @@
+<!-- 
+    newsTitle   :該頁面的主題
+    noteList    :帖子列表
+    magzineList :底部推薦的雜誌列表
+    imageLinkPathHead   :图片连接位置
+-->
+<main id="mainSection">
+	<section class="w100">
+		<div class="bar">
+			<h1>All News</h1>
+		</div>
+        <!--动态获取课程列表 -->
+        <?php 
+        foreach($noteList as $key => $value)
+        {
+        ?>
+            <div class="newsItem">
+                <span class="dateBlock"><?php print $noteList['date'] ?></span>
+                <img class="floatRight w40" src="<?php print $imageLinkPathHead.'/'.$noteList['imgurl']?>"/>
+                <div class="floatRight w100">
+                <h2><?php print $noteList['title'] ?></h2>
+                <p>
+                    <?php print $noteList['content'] ?>
+                </p>
+                </div>
+            </div>
+        <?php
+        }
+        ?>
+	</section>
+	<section class="w100">
+        <div class="bar">
+			<h1>Magazines</h1>
+		</div>
+        <!--动态获取课程列表 -->
+        <?php 
+        foreach($magzineList as $key => $value)
+        {
+        ?>
+            <div class="block <?php if(($key+1)%4==0) print "noMarginRight" ?>"> 
+                <span class="fav">喜爱</span> 
+                <img src=<?php print $imageLinkPathHead?>"/images/products/product0.jpg" />
+                <a href="#" ><h3>Magazine</h3></a>
+                <p>
+                    <span class="farmName">asdf</span>
+                    <span class="floatRight">01</span>
+                    <span class="floatRight">02</span>
+                </p>
+            </div>           
+         <?php
+         }
+         ?>
+	</section>
+</main>
