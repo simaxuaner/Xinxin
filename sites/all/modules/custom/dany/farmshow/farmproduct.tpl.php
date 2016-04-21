7ab5a33d94c9820b6865cb40055389ba33ded2c3
yunjia <yunjia_feng2016@163.com>
2016-04-21 22:05:33 +0800

    删除farmshow模块

--- sites/all/modules/custom/dany/farmshow/farmproduct.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php if ($headImg):?>
-    <img alt="header" class="headerImg" src="<?php print $headImg ?>">
-<?php endif; ?>
-<main>
-    <?php if ($farmmain): print $farmmain; endif; ?>
-</main>
