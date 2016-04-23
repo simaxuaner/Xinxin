b33c5cb20ed90e746e6cece8ddbec4c3311ed4ec
chenting123 <381559322@qq.com>
2016-04-23 20:53:13 +0800

    12

--- sites/all/modules/custom/dany/about/aboutpage.tpl.php
+++ sites/all/modules/custom/dany/about/aboutpage.tpl.php
@@ -1,4 +1,3 @@
-<img alt="header" class="headerImg" src="sites/all/modules/custom/dany/about/images/header0.jpg">
 <main id="mainSection">
 	<?php if($sidebarleft):print $sidebarleft;endif;?>
     <?php if ($pagemain): print render($pagemain); endif; ?>
