05efb9ab8a9ac3b2c9195f2bfb89d71f4b3e55da
IFEIFEI <ifeifei@stu.xmu.edu.cn>
2016-04-25 12:50:55 +0800

    dd

---
+++ sites/all/modules/custom/dany/news/newsHead.tpl.php
@@ -0,0 +1,19 @@
+<!--
+    info:array  :news66 66论坛的简介
+                :news1212 1212电商论坛简介
+                ：newsxinxin 海促会资讯信息简介
+-->
+<main id="mainSection">
+	<?php print $secondMenuNav ?>
+	<section class="w80">
+		<div class="bar">
+			<h1>品牌论坛</h1>
+		</div>
+		<h2 class="w100">6.6涂岭论坛</h2>
+		<p><?php print $info['news66'] ?></p><br>
+		<h2 class="w100">12.12电商论坛</h2>
+		<p><?php print $info['news1212'] ?></p><br>
+		<h2 class="w100">海促会资讯</h2>
+		<p<?php print $info['newsXinxin'] ?></p>
+	</section>
+</main>
