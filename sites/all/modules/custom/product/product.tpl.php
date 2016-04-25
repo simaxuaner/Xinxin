9b78e92882afd6015e0addfd307ff2ae0d84fc06
yunjia <yunjia_feng2016@163.com>
2016-04-25 17:58:23 +0800

    园区风貌修改

--- sites/all/modules/custom/product/product.tpl.php
+++ sites/all/modules/custom/product/product.tpl.php
@@ -9,8 +9,8 @@
 <main>
     <nav id="sideNav" class="w20">
         <ul>
-            <li class="activeNavItem"> <a href="<?php print $farm_page?>">农庄</a> </li>
-            <li> <a href="<?php print $product_page?>">农产品</a> </li>
+            <li> <a href="<?php print $farm_page?>">农庄</a> </li>
+            <li class="activeNavItem"> <a href="<?php print $product_page?>">农产品</a> </li>
         </ul>
     </nav>
     <section class="w80">
