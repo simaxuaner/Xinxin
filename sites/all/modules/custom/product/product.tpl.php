e745e3389876e707068d557129c648c761dd9d21
IFEIFEI <ifeifei@stu.xmu.edu.cn>
2016-06-29 17:13:36 +0800

    dd

--- sites/all/modules/custom/product/product.tpl.php
+++ sites/all/modules/custom/product/product.tpl.php
@@ -9,8 +9,8 @@
 <main>
     <nav id="sideNav" class="w20">
         <ul>
-            <li> <a href="<?php print $farm_page?>">休闲农庄</a> </li>
             <li class="activeNavItem"> <a href="<?php print $product_page?>">农业展品</a> </li>
+            <li> <a href="<?php print $farm_page?>">休闲农庄</a> </li>
         </ul>
     </nav>
     <section class="w80">
